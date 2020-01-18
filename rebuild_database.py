8c92a93b0652ee574f3d3c7f27fa1246468b3cf3
Mason <masonjuarez@pm.me>
2020-01-18 00:25:58 -0800

    should fix #7

--- rebuild_database.py
+++ rebuild_database.py
@@ -1,10 +1,21 @@
 from extbot.messages_collector import MessagesCollector
-
+import os
 #recreates (or initially creates) the database
 
 m_c = MessagesCollector()
 
+curr_db = os.path.join('data','exdatabase.sqlite3')
+os.remove(curr_db)
+
+print("Working with Group:")
+print(m_c.get_group_info_message())
+print("This may take some time!")
+total = m_c.get_total_messages()
+print("Your group has %s messages, this may take at least %s minutes." % (total, total/6000))
+print("Requesting messages...")
 m_c.get_messages()
 m_c.update_names()
 m_c.get_likes()
 m_c.get_kicks()
+
+print("Complete!")
